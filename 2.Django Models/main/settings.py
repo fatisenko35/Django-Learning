be5ba92863eaad7dff7c172a42e8c78188fa7bac
fatisenko <fatihsenko@gmail.com>
2022-05-14 19:51:27 +0300

    c1

--- 2.Django Models/main/settings.py
+++ 2.Django Models/main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h1$2p-whfc_nyk%@-hn*blslltaqgv%l#3ly4%wj^jv1#5c8k8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
